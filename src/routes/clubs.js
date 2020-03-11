e974d6fd527acc5e4c9884cf9f49003b96385490
danicelistobon <danielct-97@hotmail.com>
2020-03-11 00:15:38 -0500

    routes

---
+++ src/routes/clubs.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+
+const db = require('../database');
+
+router.get('/', async (req, res) => {
+    const clubs = await db.query('SELECT * FROM clubs');
+    res.render('clubs/home', {clubs});
+});
+
+router.get('/add', (req, res) => {
+    res.render('clubs/add');
+});
+
+router.post('/add', async (req, res) => {
+    const { name, address, neighborhood_city, club_type, open_doors, rooms, musical_genres, keywords, description } = req.body;
+    const newClub = {
+        name,
+        address,
+        neighborhood_city,
+        club_type,
+        open_doors,
+        rooms,
+        musical_genres,
+        keywords,
+        description
+    };
+    await db.query('INSERT INTO clubs set ?', [newClub]);
+    res.redirect('/clubs');
+});
+
+module.exports = router;
