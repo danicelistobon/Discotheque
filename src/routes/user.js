5b9ae65bf04aad4faf1c7e0e2e420f6c66829302
danicelistobon <danielct-97@hotmail.com>
2020-03-12 22:50:32 -0500

    added route to edit and delete a club

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,8 +1,41 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.render('user/user');
+const db = require('../database');
+
+router.get('/', async (req, res) => {
+    const clubs = await db.query('SELECT * FROM clubs');
+    res.render('user/user', {clubs: clubs});
+});
+
+router.get('/edit/:id', async (req, res) => {
+    const {id} = req.params;
+    const club = await db.query('SELECT * FROM clubs WHERE id = ?', [id]);
+    res.render('clubs/edit', {club: club[0]});
+});
+
+router.post('/edit/:id', async (req, res) => {
+    const {id} = req.params;
+    const { name, address, neighborhood_city, club_type, open_doors, rooms, musical_genres, keywords, description } = req.body;
+    const editClub = {
+        name,
+        address,
+        neighborhood_city,
+        club_type,
+        open_doors,
+        rooms,
+        musical_genres,
+        keywords,
+        description
+    };
+    await db.query('UPDATE clubs set ? WHERE id = ?', [editClub, id]);
+    res.redirect('/user');
+});
+
+router.get('/delete/:id', async (req, res) => {
+    const {id} = req.params;
+    await db.query('DELETE FROM clubs WHERE id = ?', [id]);
+    res.redirect('/user');
 });
 
 module.exports = router;
