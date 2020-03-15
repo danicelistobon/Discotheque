175acf47d9df26ee05f67ca79c00e8a95f8fe069
danicelistobon <danielct-97@hotmail.com>
2020-03-15 18:15:08 -0500

    added route sign in with passport

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -3,6 +3,17 @@ const router = express.Router();
 
 const passport = require('passport');
 
+router.get('/signin', (req, res) => {
+    res.render('auth/signin');
+});
+
+router.post('/signin', (req, res, next) => {
+    passport.authenticate('local.signin', {
+        successRedirect: '/user',
+        failureRedirect: '/signin'
+    })(req, res, next);
+});
+
 router.get('/signup', (req, res) => {
     res.render('auth/signup');
 });
