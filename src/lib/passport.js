b45221911fb2a1356d56ba894352bc9224b703a6
danicelistobon <danielct-97@hotmail.com>
2020-03-15 18:14:46 -0500

    added route sign in with passport

--- src/lib/passport.js
+++ src/lib/passport.js
@@ -4,6 +4,26 @@ const localStrategy = require('passport-local').Strategy;
 const db = require('../database');
 const helpers = require('../lib/helpers');
 
+passport.use('local.signin' , new localStrategy({
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true
+}, async (req, username, password, done) => {
+    console.log(req.body);
+    const rows = await db.query('SELECT * FROM users WHERE email = ?', [username]);
+    if (rows.length > 0) {
+        const user = rows[0];
+        const validPwd = await helpers.comparePassword(password, user.password);
+        if (validPwd) {
+            done(null, user);
+        } else {
+            done(null, false);
+        }
+    } else {
+        return done(null, false);
+    }
+}));
+
 passport.use('local.signup', new localStrategy({
     usernameField: 'email',
     passwordField: 'password',
