b4ba44ef141facf2ffb4f7be3d6a5250026fd07b
danicelistobon <danielct-97@hotmail.com>
2020-03-15 17:05:53 -0500

    modified route with passport

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+const passport = require('passport');
+
 router.get('/signup', (req, res) => {
     res.render('auth/signup');
 });
 
-router.post('/signup', (req, res) => {
-    console.log(req.body);
-    res.send('received');
-});
+router.post('/signup', passport.authenticate('local.signup', {
+    successRedirect: '/user',
+    failureRedirect: '/signup'
+}));
 
 module.exports = router;
