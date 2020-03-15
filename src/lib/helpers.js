a4211afdc2cde52d04f3523ef71be45233c665fb
danicelistobon <danielct-97@hotmail.com>
2020-03-15 17:06:56 -0500

    added methods to encrypt and compare the password

---
+++ src/lib/helpers.js
@@ -0,0 +1,19 @@
+const bcrypt = require('bcryptjs');
+
+const helpers = {};
+
+helpers.encryptPassword = async (password) => {
+    const salt = await bcrypt.genSalt(10);
+    const pwdHash = await bcrypt.hash(password, salt);
+    return pwdHash;
+};
+
+helpers.comparePassword = async (password, savedPwd) => {
+    try {
+        await bcrypt.compare(password, savedPwd);
+    } catch(err) {
+        console.log(err);
+    }
+};
+
+module.exports = helpers;
