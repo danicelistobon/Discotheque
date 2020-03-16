21503ed4bdc6fc61a30a9ac189751dcf90486bed
danicelistobon <danielct-97@hotmail.com>
2020-03-15 20:14:33 -0500

    added isSignedIn helper to sign in and sign up routes, added isNotSignedIn helper to sign out route

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -2,28 +2,29 @@ const express = require('express');
 const router = express.Router();
 
 const passport = require('passport');
+const { isSignedIn, isNotSignedIn } = require('../lib/auth');
 
-router.get('/signin', (req, res) => {
+router.get('/signin', isSignedIn, (req, res) => {
     res.render('auth/signin');
 });
 
-router.post('/signin', (req, res, next) => {
+router.post('/signin', isSignedIn, (req, res, next) => {
     passport.authenticate('local.signin', {
         successRedirect: '/user',
         failureRedirect: '/signin'
     })(req, res, next);
 });
 
-router.get('/signup', (req, res) => {
+router.get('/signup', isSignedIn, (req, res) => {
     res.render('auth/signup');
 });
 
-router.post('/signup', passport.authenticate('local.signup', {
+router.post('/signup', isSignedIn, passport.authenticate('local.signup', {
     successRedirect: '/user',
     failureRedirect: '/signup'
 }));
 
-router.get('/signout', (req, res) => {
+router.get('/signout', isNotSignedIn, (req, res) => {
     req.logOut();
     res.redirect('/');
 });
