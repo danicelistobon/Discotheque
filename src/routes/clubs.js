047a0181465cb5fa67a92519393645ce3252cdea
danicelistobon <danielct-97@hotmail.com>
2020-03-15 19:55:53 -0500

    added isNotSignedIn helper to add routes

--- src/routes/clubs.js
+++ src/routes/clubs.js
@@ -2,17 +2,18 @@ const express = require('express');
 const router = express.Router();
 
 const db = require('../database');
+const { isNotSignedIn } = require('../lib/auth');
 
 router.get('/', async (req, res) => {
     const clubs = await db.query('SELECT * FROM clubs');
     res.render('clubs/home', {clubs: clubs});
 });
 
-router.get('/add', (req, res) => {
+router.get('/add', isNotSignedIn, (req, res) => {
     res.render('clubs/add');
 });
 
-router.post('/add', async (req, res) => {
+router.post('/add', isNotSignedIn, async (req, res) => {
     const { name, address, neighborhood_city, club_type, open_doors, rooms, musical_genres, keywords, description } = req.body;
     const newClub = {
         name,
