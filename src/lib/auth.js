0c3b559fa4c50715d473797e47b069d8775bfdfe
danicelistobon <danielct-97@hotmail.com>
2020-03-15 19:54:48 -0500

    created isNotSignedIn helper

---
+++ src/lib/auth.js
@@ -0,0 +1,8 @@
+module.exports = {
+    isNotSignedIn (req, res, next) {
+        if (req.isAuthenticated()) {
+            return next();
+        }
+        return res.redirect('/signin');
+    }
+};
