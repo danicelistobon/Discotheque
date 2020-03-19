183167833037c4accf03c3d39fcc553ce3fb4950
danicelistobon <danielct-97@hotmail.com>
2020-03-18 22:14:28 -0500

    modified db

--- database/db.sql
+++ database/db.sql
@@ -43,3 +43,39 @@ ALTER TABLE clubs
     MODIFY id INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 333;
 
 DESCRIBE clubs;
+
+CREATE TABLE events(
+    id INT(11) NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    place VARCHAR(100) NOT NULL,
+    date_event VARCHAR(50) NOT NULL,
+    cover VARCHAR(11) NOT NULL,
+    description TEXT NOT NULL,
+    user_id INT(11) NOT NULL,
+    created_at timestamp NOT NULL DEFAULT current_timestamp,
+    CONSTRAINT fk_club FOREIGN KEY(user_id) REFERENCES users(id)
+);
+
+ALTER TABLE events
+    ADD PRIMARY KEY (id);
+
+ALTER TABLE events
+    MODIFY id INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 333;
+
+DESCRIBE events;
+
+CREATE TABLE tickets(
+    id INT(30) NOT NULL,
+    id_card VARCHAR(30) NOT NULL,
+    user_id INT(11) NOT NULL,
+    created_at timestamp NOT NULL DEFAULT current_timestamp,
+    CONSTRAINT fk_ticket FOREIGN KEY(user_id) REFERENCES users(id)
+);
+
+ALTER TABLE tickets
+    ADD PRIMARY KEY (id);
+
+ALTER TABLE tickets
+    MODIFY id INT(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 111111;
+
+DESCRIBE tickets;
