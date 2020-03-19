a65ba7a0a1b8c76b4ffef88a4a46c9561a48cb61
danicelistobon <danielct-97@hotmail.com>
2020-03-18 22:18:11 -0500

    modified user routes

--- src/routes/user.js
+++ src/routes/user.js
@@ -6,7 +6,8 @@ const { isNotSignedIn } = require('../lib/auth');
 
 router.get('/', isNotSignedIn, async (req, res) => {
     const clubs = await db.query('SELECT * FROM clubs WHERE user_id = ?', [req.user.id]);
-    res.render('user/user', {clubs: clubs});
+    const events = await db.query('SELECT * FROM events WHERE user_id = ?', [req.user.id]);
+    res.render('user/user', {clubs: clubs, events: events});
 });
 
 router.get('/edit/:id', isNotSignedIn, async (req, res) => {
@@ -36,6 +37,33 @@ router.post('/edit/:id', isNotSignedIn, async (req, res) => {
 router.get('/delete/:id', isNotSignedIn, async (req, res) => {
     const {id} = req.params;
     await db.query('DELETE FROM clubs WHERE id = ?', [id]);
+    res.redirect('/user');
+});
+
+router.get('/edit/event/:id', isNotSignedIn, async (req, res) => {
+    const {id} = req.params;
+    const event = await db.query('SELECT * FROM events WHERE id = ?', [id]);
+    res.render('clubs/edit_event', {event: event[0]});
+});
+
+router.post('/edit/event/:id', isNotSignedIn, async (req, res) => {
+    const {id} = req.params;
+    const { name, place, date_event, cover, description } = req.body;
+    const editEvent = {
+        name,
+        place,
+        date_event,
+        cover,
+        description,
+        user_id: req.user.id
+    };
+    await db.query('UPDATE events set ? WHERE id = ?', [editEvent, id]);
+    res.redirect('/user');
+});
+
+router.get('/delete/event/:id', isNotSignedIn, async (req, res) => {
+    const {id} = req.params;
+    await db.query('DELETE FROM events WHERE id = ?', [id]);
     res.redirect('/user');
 });
 
