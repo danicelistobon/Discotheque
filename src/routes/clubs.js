3976b9c912472439c0f9ad6386d263f1cc8cf693
danicelistobon <danielct-97@hotmail.com>
2020-03-18 22:17:39 -0500

    modified clubs routes

--- src/routes/clubs.js
+++ src/routes/clubs.js
@@ -34,7 +34,41 @@ router.post('/add', isNotSignedIn, async (req, res) => {
 router.get('/club/:id', async (req, res) => {
     const {id} = req.params;
     const club = await db.query('SELECT * FROM clubs WHERE id = ?', [id]);
-    res.render('clubs/club_profile', {club: club[0]});
+    const club_name = club[0].name;
+    const club_user_id = club[0].user_id;
+    console.log(club_user_id);
+    const events = await db.query('SELECT * FROM events WHERE place = ? AND user_id = ?', [club_name, club_user_id]);
+    res.render('clubs/club_profile', {club: club[0], events: events});
+});
+
+router.get('/event/:id', async (req, res) => {
+    const {id} = req.params;
+    const event = await db.query('SELECT * FROM events WHERE id = ?', [id]);
+    res.render('clubs/event', {event: event[0]});
+});
+
+router.get('/add/event', isNotSignedIn, (req, res) => {
+    res.render('clubs/add_event');
+});
+
+router.post('/add/event', isNotSignedIn, async (req, res) => {
+    const { name, place, date_event, cover, description } = req.body;
+    const newEvent = {
+        name,
+        place,
+        date_event,
+        cover,
+        description,
+        user_id: req.user.id
+    };
+    await db.query('INSERT INTO events set ?', [newEvent]);
+    res.redirect('/user');
+});
+
+router.get('/ticket/:id', async (req, res) => {
+    const {id} = req.params;
+    const ticket = await db.query('SELECT * FROM tickets WHERE user_id = ?', [id]);
+    res.render('clubs/ticket', {ticket: ticket});
 });
 
 module.exports = router;
